8e8645b5bc8347edc1669ebe90abe82e22a0dc9b
Mathew Cullen <matcullen88@gmail.com>
2016-10-06 16:17:14 -0400

    added randomization functionality for obstacle speed and height

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ var carHeight = parseInt(car.css('top'));
 var carWidth = parseInt(car.css('right'));
 var player1 = $('#player1')
 // var player2 = $('#player2')
-var speed = 10;
-var gameOver = false;
 
+var speed = 10;
+var score = 0;
+var gameOver = false;
+var gamePaused = false;
+var pauseButton = $('pauseButton')
+var restartButton = $('restartButton')
 
 var obstacle = $('.obstacle');
 var obstacle_1 = $('#obstacle_1');
@@ -20,38 +24,43 @@ var obstacle_6 = $('#obstacle_6');
 
 var boardHeight = parseInt(board.height());
 var boardWidth = parseInt(board.width());
+var carHeight = parseInt(car.height());
+var carWidth = parseInt(car.width());
 var obstacleInitialPostion = parseInt(obstacle.css('top'));
 // var obstacleCurrentPosition = parseInt(obstacle.css('top'));
+
 
 var startGame = setInterval (function(){
   // this will update the obstacle's current position every 50ms
   obstacleCurrentPosition = parseInt(obstacle.css('top'));
-  console.log(obstacleCurrentPosition);
-
   // when current > 700px, current resets to a random height;
   if(obstacleCurrentPosition > boardHeight) {
     obstacleCurrentPosition = obstacleInitialPostion;
-    obstacle.css('top', nextObstacleHeight(-200, -500));
-
+    obstacle.css('top', randomizeObstacle(-200, -500));
+    obstacle.css('right', randomizeObstacle(0, 100));
+    speed = randomizeObstacle(8,12);
   } else{
     obstacle.css('top', obstacleCurrentPosition + speed);
+    console.log(speed);
   }
 
-  if (parseInt(car.css('top')) <= 0 || parseInt(car.css('top')) > parseInt(board.height() - 100)){
+  if (parseInt(car.css('top')) <= 0 || parseInt(car.css('top')) >= boardHeight - carHeight || parseInt(car.css('right')) <= 0 || parseInt(car.css('right')) >= boardWidth - carWidth){
   gameOver = true;
-  clearInterval();
   }
 
   if (gameOver === true){
-  console.log('Game Over')};
-
-  nextObstacleHeight = function (min, max){
-    min=Math.ceil(min);
-    max=Math.floor(max);
-    return Math.floor(Math.random() * (max-min)) + min;
-  }
+    clearInterval(startGame);
+    alert('Game Over')
+    board.css(animation, none);
+    };
 
 }, 50);
+
+randomizeObstacle = function (min, max){
+  min=Math.ceil(min);
+  max=Math.floor(max);
+  return Math.floor(Math.random() * (max-min)) + min;
+}
 
 $(document).on('keydown', function(event){
   var key = event.keyCode;
@@ -90,6 +99,8 @@ $(document).on('keydown', function(event){
 
 
 
+
+
 // sets the obstacle's position
 // checks if the obstacle is still contained within the height
 // sets the current position equal to where it was starting the game to return the div
@@ -98,27 +109,34 @@ $(document).on('keydown', function(event){
 
 // var nextObstacle = parseInt(Math.random() * 10);
 
+// pauseButton.click(pauseGame());
+//   gamePaused = true;
+
+// function(pauseGame){
+//   var scoreStored;
+//   clearInterval(startGame);
+//   scoreStored = score;
+// }
+
+// function(resumeGame){
+//   startGame();
+//   score = scoreStored;
+// }
 
 // function collision($div1, $div2){
-//   var x1 = $div1.offset().left;
-//   var y1 = $div1.offset().top;
-//   var h1 = $div1.outerHeight(true);
-//   var w1 = $div1.outerWidth(true);
+//   var x1 = obstacle.offset().left;
+//   var y1 = obstacle.offset().top;
+//   var h1 = obstacle.outerHeight(true);
+//   var w1 = obstacle.outerWidth(true);
 //   var b1 = y1 + h1;
 //   var r1 = x1 + w1;
-//   var x2 = $div2.offset().left;
-//   var y2 = $div2.offset().top;
-//   var h2 = $div2.outerHeight(true);
-//   var w2 = $div2.outerWidth(true);
-//   var b2 = y2 + h2;
-//   var r2 = x2 + w2;
 
 //   if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
 //   return true;
 // }
 
 // if (car.right > obstacle.left && car.left < obstacle.right && car.top < obstacle.bottom && car.bottom > obstacle.top {
-//   return false;
+//   gameOver = true;
 // }
 
 // // resets the obstacles to random heights when each leaves the bottom of the game area
